b693c8259d01fc6b14ac46a43d91f798373f041b
carloadamos <johncarloadamos@gmail.com>
2021-03-05 16:50:39 +0800

    Postgresql

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { testEnvironmentVariable } from '../settings';
+import { messagesPage } from '../controllers/messages';
 
 const indexRouter = express.Router();
 
 indexRouter.get('/', (req, res) => res.status(200).json({ message: testEnvironmentVariable }));
+indexRouter.get('/messages', messagesPage);
 
 export default indexRouter;
